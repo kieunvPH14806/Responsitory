d6f366acb4bbae25ce8cf10220e30e47776bf925
Nguyen Kieu <vankieu0409@gmail.com>
2022-03-23 16:46:26 +0700

    commit

--- Program.cs
+++ Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddScoped<IClassRepository, ClassRepository>();
 builder.Services.AddScoped<IStudentRepository, StudentRespository>();
 builder.Services.AddScoped<IClassService, ClassService>();
 builder.Services.AddScoped<IStudentService, StudentService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
